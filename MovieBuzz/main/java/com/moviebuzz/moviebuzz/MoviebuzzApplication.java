10873f58444c19cef14a3ac59d0a4929af6734b1
Vinay Darapureddi <58486882+vinaydarapureddi@users.noreply.github.com>
2020-10-05 22:49:57 +0200

    Add files via upload

---
+++ MovieBuzz/main/java/com/moviebuzz/moviebuzz/MoviebuzzApplication.java
@@ -0,0 +1,13 @@
+package com.moviebuzz.moviebuzz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviebuzzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviebuzzApplication.class, args);
+	}
+
+}
---
+++ MovieBuzz/test/java/com/moviebuzz/moviebuzz/MoviebuzzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moviebuzz.moviebuzz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviebuzzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
